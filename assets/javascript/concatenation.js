af0f0ed003eccada210768eeb5af3d4ed979edf5
DeepakK <khamkardeepak7@gmail.com>
2024-03-02 01:25:01 +0530

    Add JavaScript changes for operators and cancate.

---
+++ assets/javascript/concatenation.js
@@ -0,0 +1,16 @@
+/**
+ * Concatenation
+ */
+
+let userName = "Deepak";
+let age = 45;
+
+let message = "My name is " + userName;
+console.log(message);
+
+let text = `My name is ${userName} and I am ${age} years old.`;
+console.log(text);
+
+let multilineText = `My name is ${userName} and 
+I am ${age} years old.`;
+console.log(multilineText);
---
+++ assets/javascript/operators.js
@@ -0,0 +1,34 @@
+/**
+ * JS Operators
+ */
+
+const x= 12;
+const y=3;
+
+console.log(x + y);
+console.log(x - y);
+console.log( x * y);
+console.log( x / y );
+
+console.log(x % y );
+console.log(x ** 2);
+
+/**
+ * We are checking the string operator
+ */
+let x1 = "12";
+let y1 = "3";
+console.log(x1 + y1);
+console.log(x1 - y1);
+console.log(x1 * y1);
+console.log(x1 / y1);
+console.log(x1 % y1);
+
+
+const a1 = "Apple";
+const b1 = "Mango";
+console.log(a1 + b1);
+console.log(a1 - b1);
+console.log(a1 * b1);
+console.log(a1 / b1);
+console.log(a1 % b1);
