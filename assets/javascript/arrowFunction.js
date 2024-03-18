fd76cd9d72f4bf8333203c121c34db5e31f41211
DeepakK <khamkardeepak7@gmail.com>
2024-03-18 23:40:49 +0530

    Adding JS function example.

---
+++ assets/javascript/arrowFunction.js
@@ -0,0 +1,14 @@
+/**
+ * Function Use: Example of Arrow Function
+ */
+
+const addFunction = () => {
+    let a = 5, b=6;
+    return a+b;
+}
+
+console.log(addFunction());
+
+///Number multiplication
+const MultiNumber = num => num * num;
+console.log(MultiNumber(5));
---
+++ assets/javascript/jsFunctions.js
@@ -0,0 +1,45 @@
+/**
+ * JavaScript Functions:
+ */
+
+//Function to calculate the addition
+function addNumber(number1, number2){
+    console.log(`Addition of ${number1} and ${number2} is ${number1 + number2}`);
+}
+
+addNumber(5,6);
+
+function subNumber(Num1, Num2){
+    console.log(`Subtraction of ${Num1} and ${Num2} is ${Num1 - Num2}`);
+}
+
+subNumber(10, 5);
+
+/**
+ * Anonymous function
+ */
+var greet = function(){
+    console.log("Welcome to Greet!");
+};
+
+greet();
+
+//Anonymous function with parameter
+var greetWithParam = function(string1) {
+    console.log("Welcome "+string1);
+};
+
+greetWithParam("Deepak");
+
+//function with settimeout method
+setTimeout( function(){
+    console.log("Welcome with setTimeout function ");
+}, 2000);
+
+//Arrow function
+var arrowFunc = () => {
+    console.log("Welcome to Arrow Function!!");
+}
+
+//call the arrow function
+arrowFunc();
