3ac4c870b592f9507a05d291a8a481ddbd990fbd
DeepakK <khamkardeepak7@gmail.com>
2024-03-25 15:03:37 +0530

    Adding JS updated basic code changes.

--- assets/javascript/firstBasic.js
+++ assets/javascript/firstBasic.js
@@ -4,4 +4,39 @@ Topic: We are writing basic javascript
 console.log("Hello World!!");
 
 console.log("My name is Deepak");
-console.log(7+3);
+console.log(7+43);
+
+
+//
+// console.log(a);
+// var a =10;
+
+// function abc(){
+//     a =100;
+//     console.log(a);
+// }
+
+// console.log(a);
+
+// abc();
+
+// console.log(a);
+
+//Exercise
+var a =10;
+function first(){
+    function second(){
+        console.log(a);
+        var a = 99;
+            function third(){
+                console.log(a);
+            }
+            third();
+    }
+    var a = 100;
+    second();
+    console.log(a);
+}
+
+first();
+console.log(a);
