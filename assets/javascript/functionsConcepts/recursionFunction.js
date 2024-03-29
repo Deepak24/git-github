aac3c7dab5e9b2f0290afb2e41c81a1aad422000
DeepakK <khamkardeepak7@gmail.com>
2024-03-30 01:00:52 +0530

    Add recursion file changes.

--- assets/javascript/functionsConcepts/recursionFunction.js
+++ assets/javascript/functionsConcepts/recursionFunction.js
@@ -22,9 +22,36 @@ function calculateSum(number1 ) {
 const result = calculateSum(10);
 console.log(result);
 
-function calculateRecursiveSum() {
-
+function calculateRecursiveSum(number1) {
+    if( number1 === 1 )
+        return number1;
+    return number1 + calculateRecursiveSum(number1 - 1);
 }
+
+//How recursion takes place as below
+//10 + calculateRecursiveSum(9)
+//10 + 9 + calculateRecursiveSum(8)
+//10 + 9+ 8 + calculateRecursiveSum(7)
+//10 + 9 + 8 + 7 + calculateRecursiveSum(6)
+//10 + 9 + 8 + 7 + 6 + calculateRecursiveSum(5)
+//10 + 9 + 8 + 7 + 6 + 5 + calculateRecursiveSum(4)
+//10 + 9 + 8 + 7 + 6 + 5 + 4 + calculateRecursiveSum(3)
+//10 + 9 + 8 + 7 + 6 + 5 + 4 + 3 + calculateRecursiveSum(2)
+//10 + 9 + 8 + 7 + 6 + 5 + 4 + 3 + 2 + calculateRecursiveSum(1)
+//10 + 9 + 8 + 7 + 6 + 5 + 4 + 3 + 2 + 1
 
 const totalSum = calculateRecursiveSum(10);
 console.log(totalSum);
+
+//Factorial of Number 5 --> 5x4x3x2x1 = 120
+//number x (number -1) x (number - 2) x (number - 3) x (number - 4)
+
+function factorialRecursive(number2) {
+    if ( number2 === 1)
+        return number2;
+
+    return number2 * factorialRecursive(number2 - 1);
+}
+
+const factResult = factorialRecursive(5);
+console.log(factResult);
