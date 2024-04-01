c6f196ac22eb5c59ed4f39edec624a874a344b0b
DeepakK <khamkardeepak7@gmail.com>
2024-04-01 20:21:56 +0530

    Change folder structure for DOM files and added new DOM searching file.

---
+++ assets/javascript/assetsToExternalFile/domSearch.js
@@ -0,0 +1,20 @@
+/**
+ * Description: This is external JS or asset file of domSearching file
+ */
+// let startBtn = document.getElementById("btnSubmit");
+// console.log(startBtn);
+
+// let stopBtn = document.getElementById("btnReset");
+// console.log(stopBtn);
+
+//We can use querySelector for getting elements 
+let startBtn = document.querySelector(".start");
+console.log(startBtn);
+
+let stopBtn = document.querySelector("#btnReset");
+console.log(stopBtn);
+
+//With querySelectorAll
+
+let getBtn = document.querySelectorAll(".button");
+console.log(getBtn);
