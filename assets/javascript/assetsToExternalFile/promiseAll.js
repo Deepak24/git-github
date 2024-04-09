0b046991f8a67059d0d3dca43fccd90ec9ce57f8
DeepakK <khamkardeepak7@gmail.com>
2024-04-09 13:19:12 +0530

    Add updated promise file changes.

--- assets/javascript/assetsToExternalFile/promiseAll.js
+++ assets/javascript/assetsToExternalFile/promiseAll.js
@@ -2,15 +2,15 @@
  * Description: Promise All (array of promises)
  */
 let promise1 = new Promise((resolve) => {
-    setTimeout(() => resolve("Promise 1 is resolved"), 1000);
+    setTimeout(() => resolve("Promise 1 is resolved"), 400);
 });
 
 let promise2 = new Promise((resolve) => {
-    setTimeout(() => resolve("Promise 2 is resolved"), 2000);
+    setTimeout(() => resolve("Promise 2 is resolved"), 600);
 });
 
 let promise3 = new Promise((resolve) => {
-    setTimeout(() => resolve("Promise 3 is resolved"), 3000);
+    setTimeout(() => resolve("Promise 3 is resolved"), 300);
 });
 
 promise1.then((res) => console.log(res));
@@ -20,3 +20,4 @@ promise3.then((res) => console.log(res));
 let arrPromises = [promise1, promise2, promise3];
 let promise = Promise.all(arrPromises);
 console.log(arrPromises);
+console.log(promise);
