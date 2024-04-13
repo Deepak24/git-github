dcb1271b3142cd737783905892d67f809f6b3efb
DeepakK <khamkardeepak7@gmail.com>
2024-04-13 15:31:19 +0530

    Adding second React app with function component.

---
+++ ReactApplications/functiondemo/src/component/Posts.jsx
@@ -0,0 +1,19 @@
+import useFetchData from "../hooks/FetchDataHook";
+const Posts = () => {
+    const URL = "https://jsonplaceholder.typicode.com/posts";
+    const postData = useFetchData(URL);
+    return(
+        <>
+            <h2>Fetch Posts</h2>
+            {postData?.map(post => 
+                <div key ={post.id}>
+                    <h4>{post.title}</h4>
+                    <p> {post.body} </p>
+                    <hr />
+                </div>
+            )}
+        </>
+    )
+}
+
+export default Posts
