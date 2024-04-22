594f50c52f4f9e04c2b130fd52f30b14e56e86a0
DeepakK <khamkardeepak7@gmail.com>
2024-04-23 00:06:15 +0530

    Adding react state management and route file changes.

---
+++ ReactApplications/statemanagementroute/src/App.js
@@ -0,0 +1,12 @@
+import MyComponent from "./mycomponent";
+
+function App() {
+  return (
+    <div className="App">
+        <h2>We are in parent or primary component.</h2>
+        <MyComponent />
+    </div>
+  );
+}
+
+export default App;
