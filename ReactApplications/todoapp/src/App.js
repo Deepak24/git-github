71ef388c49f53d61a8a44736c696eab7e113e1f5
DeepakK <khamkardeepak7@gmail.com>
2024-04-24 20:15:29 +0530

    Adding React toto app file changes.

---
+++ ReactApplications/todoapp/src/App.js
@@ -0,0 +1,12 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div className="App">
+        <h2>Hello World!!</h2>
+        <Todo />
+    </div>
+  );
+}
+
+export default App;
