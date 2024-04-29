fe50bc25646aaa9a054c00b59eb8fd6f6cfaf08b
DeepakK <khamkardeepak7@gmail.com>
2024-04-29 21:54:56 +0530

    Addded updated file changes for products file accessing firebase database.

--- ReactApplications/cartdemo2/src/component/products/Products.js
+++ ReactApplications/cartdemo2/src/component/products/Products.js
@@ -8,36 +8,7 @@ import axios from "axios";
 
 const Products = () => {
     
-    const [item, setItems] = useState([
-        // {
-        //     id : 0,
-        //     title : "Title of the item 1",
-        //     price : 450,
-        //     discountedPrice : 320,
-        //     thumbnail : "200x150.png"
-        // },
-        // {
-        //     id : 1,
-        //     title : "Title of the item 2",
-        //     price : 550,
-        //     discountedPrice : 420,
-        //     thumbnail : "200x150.png"
-        // },
-        // {
-        //     id : 2,
-        //     title : "Title of the item 3",
-        //     price : 850,
-        //     discountedPrice : 630,
-        //     thumbnail : "200x150.png"
-        // },
-        // {
-        //     id : 3,
-        //     title : "Title of the item 4",
-        //     price : 490,
-        //     discountedPrice : 360,
-        //     thumbnail : "200x150.png"
-        // }
-    ]);
+    const [item, setItems] = useState([]);
 
     //We are getting callback hell condition here - We have replaced it with chaining process
     useEffect(() => {
