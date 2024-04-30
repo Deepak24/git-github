111815f942c85d9a5e422d1fa488a3c35bc51d7c
DeepakK <khamkardeepak7@gmail.com>
2024-05-01 02:02:27 +0530

    Added Test exercise 7 file changes.

---
+++ assets/javascript/Exercise/Tests/JSTestExercise7.js
@@ -0,0 +1,44 @@
+/**
+ * Description: JavaScript Question and answer - Exercise 6
+ */
+//1
+for(let i=1;i<=3;i++) {
+	setTimeout(function (){
+		console.log(i);
+	}, Math.random() *1000);
+}
+/* Output
+3
+1
+2
+*/
+
+//2
+const object1 = {
+
+	property1: 20
+
+};
+
+console.log(Object.is(object1));//false
+
+//3
+var x = 1;
+
+function func(){
+   
+	if (!x){
+		var x=2;
+	}
+
+	console.log(x);
+}
+
+func();//2
+
+
+//4
+const event = new Date(Date.UTC(2012, 11, 20, 3, 0, 0));
+
+
+console.log (event.toLocaleString('en-IN', { timeZone: 'UTC' }));//20/12/2012, 3:00:00 am
