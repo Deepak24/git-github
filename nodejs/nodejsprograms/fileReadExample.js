bdc332cb46847de21cd883647ee2b33e4b5d4425
DeepakK <khamkardeepak7@gmail.com>
2024-05-09 21:49:39 +0530

    Adding nodejs callback function file changes.

--- nodejs/nodejsprograms/fileReadExample.js
+++ nodejs/nodejsprograms/fileReadExample.js
@@ -4,9 +4,15 @@
 
 import fs from "fs";
 const path = "./nodejs/nodejsprograms/data/deepak/input.txt";
+//Here we have used readFile which asynchrounously reads the file 
 fs.readFile(path, function(ferr, filedata) {
     if(ferr) return console.log(ferr);
     console.log(filedata.toString());  
 });
 
 console.log("End of file program execution");
+
+//Read file with readFileSync method
+const strPath = "./nodejs/nodejsprograms/data/inputFile.txt";
+const fileData = fs.readFileSync(strPath);//Synchronous function
+console.log(fileData.toString());
