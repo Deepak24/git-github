8b04e9deadebdc8a87c6471fb3b78754b1cdeda5
DeepakK <khamkardeepak7@gmail.com>
2024-05-25 00:56:43 +0530

    Add practice problem solutions.

--- assets/javascript/Exercise/practiceProblems/functionExamples.js
+++ assets/javascript/Exercise/practiceProblems/functionExamples.js
@@ -15,9 +15,9 @@ function factorial(n){
 }
 
 const numFactorial = factorial(25);
-console.log(numFactorial);
+console.log("Factorial " + numFactorial);// 1.5511
 
-//2. Finx One extra character
+//2. Find One extra character
 function findExtraCharacter(s1, s2) {
     let l1 = s1.length;
     let l2 = s2.length;
@@ -37,22 +37,19 @@ function findExtraCharacter(s1, s2) {
 
 let str1 = "abcdef";
 let str2 = "gabcdef";
-console.log(findExtraCharacter(str1, str2));
+console.log(findExtraCharacter(str1, str2));//Output - f
 
-//
+//3 - Print the characters in between given 2 characters
 let char1 = "a";
 let char2 = "g";
 let intChar1Code = char1.charCodeAt(0);
 let intChar2Code = char2.charCodeAt(0);
 
-console.log(intChar1Code);
-console.log(intChar2Code);
-
 let alphabet = String.fromCharCode(...Array(intChar2Code+1).keys()).slice(intChar1Code);
-console.log(alphabet.split("").join(' '));
+alphabet = alphabet.split("").join(' ');
 
 let strChar = '';
 for(let i = intChar1Code; i <= intChar2Code; i++) {
     strChar += String.fromCharCode(i) + ' ';
 }
-console.log(strChar);
+console.log(strChar);//Output - a b c d e f g 
